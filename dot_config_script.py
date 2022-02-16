3a1ef3d4d051da32da2c2e7361a351ef499391ee
Stephen Freed <88246643+StephenFreed@users.noreply.github.com>
2022-02-15 20:05:01 -0500

    script auto push 15-02-22 20:05:00

--- dot_config_script.py
+++ dot_config_script.py
@@ -1,6 +1,6 @@
 #!/Users/stephenfreed/.config/Dot-Config-Script-Py/bin/python3
 
-from git.repo import Repo
+from git.repo import Repo  # type: ignore
 import time
 from datetime import datetime
 
@@ -10,6 +10,7 @@ dt = datetime.now().strftime('%d-%m-%y %H:%M:%S')
 COMMIT_MESSAGE = 'script auto push '
 repo = Repo(PATH_OF_GIT_REPO)
 
+
 def git_push():
     try:
         repo.git.add(A=True)
@@ -18,8 +19,10 @@ def git_push():
         time.sleep(1)
         origin = repo.remote(name='origin')
         origin.push(refspec='{}:{}'.format('main', 'main'))
-    except:
+    except:  # noqa
         with open('/Users/stephenfreed/.config/script_log_file.txt', 'a') as my_file:
             my_file.write('ERROR While Pushing Code ' + dt + '\n')
+
+
 if repo.is_dirty(untracked_files=True):
     git_push()
